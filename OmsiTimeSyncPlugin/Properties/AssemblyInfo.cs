a4806e66afc3c2d32f71ac59791957f1bec4f27c
Paul <paul.lewis@fr89.uk>
2022-01-02 16:32:43 +0000

    Updated assembly information

--- OmsiTimeSyncPlugin/Properties/AssemblyInfo.cs
+++ OmsiTimeSyncPlugin/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OmsiTimeSyncPlugin")]
+[assembly: AssemblyTitle("OMSI Time Sync Telemetry Plugin")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("OmsiTimeSyncPlugin")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyProduct("OMSI Time Sync Telemetry Plugin")]
+[assembly: AssemblyCopyright("Copyright ©Ixel 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
